15dd4e21d8dc8ac9304f57954c46a1bb13917acf
tuankyo <tuankyo.foto@gmail.com>
2019-09-06 14:46:13 +0700

    Update codebird.php
    Update version ads to 6

--- src/codebird.php
+++ src/codebird.php
@@ -68,8 +68,8 @@ class Codebird
    */
   protected static $_endpoints = [
     'ads'          => [
-      'production' => 'https://ads-api.twitter.com/3/',
-      'sandbox'    => 'https://ads-api-sandbox.twitter.com/3/'
+      'production' => 'https://ads-api.twitter.com/6/',
+      'sandbox'    => 'https://ads-api-sandbox.twitter.com/6/'
     ],
     'media'        => 'https://upload.twitter.com/1.1/',
     'publish'      => 'https://publish.twitter.com/',
